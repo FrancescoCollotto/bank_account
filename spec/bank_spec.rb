12d4251a9818d23546ad45ddfb1f0d8c2ff0ccb3
Francesco <francescocollotto@gmail.com>
2021-11-17 10:56:55 +0000

    write test to check balance is an Integer

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,6 +2,7 @@ require "bank"
 
 describe Account do
   it { expect(subject).to respond_to :balance }
+  it { expect(subject.balance).to be_an_instance_of Integer}
   it { expect(subject).to respond_to :deposit }
   it { expect(subject).to respond_to :withdraw }
 end
