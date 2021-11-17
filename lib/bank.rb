f6f39d491070a1576397588c88613c081a99224c
Francesco <francescocollotto@gmail.com>
2021-11-17 11:26:57 +0000

    add rspec to check deposit return an Integer

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,10 +1,11 @@
 class Account
-    attr_accessor :balance
+    attr_reader :balance
   def initialize
     @balance = 1000
   end
 
   def deposit(amount)
+    @balance += amount
   end
 
   def withdraw(amount)
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,5 +4,6 @@ describe Account do
   it { expect(subject).to respond_to :balance }
   it { expect(subject.balance).to be_an_instance_of Integer}
   it { expect(subject).to respond_to :deposit }
+  it { expect(subject.deposit(200)).to be_an_instance_of Integer}
   it { expect(subject).to respond_to :withdraw }
 end
