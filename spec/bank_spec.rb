7529c44df5407ab3bc3b819f9b71718e37deade3
Francesco <francescocollotto@gmail.com>
2021-11-17 10:49:48 +0000

    write rspec test to respond to withdraw method

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,4 +3,5 @@ require "bank"
 describe Account do
   it { expect(subject).to respond_to :balance }
   it { expect(subject).to respond_to :deposit }
+  it { expect(subject).to respond_to :withdraw }
 end
